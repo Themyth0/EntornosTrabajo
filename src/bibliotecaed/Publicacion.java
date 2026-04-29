41bf54e25337a8b91118fe57772dace942150b6f
Themyth0 <131913542+Themyth0@users.noreply.github.com>
2026-04-29 10:53:08 +0200

    Java clases

---
+++ src/bibliotecaed/Publicacion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bibliotecaed;
+
+/**
+ *
+ * @author usuario
+ */
+public class Publicacion {
+    private String TITULO;
+    private int FECHAPUB;
+}
